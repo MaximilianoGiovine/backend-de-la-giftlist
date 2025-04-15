30e14512a586cd71873056182eacca73faf9eebb
Maximiliano Ariel Giovine <maxiagiovine@gmail.com>
2025-04-15 11:23:10 -0300

    added a dynamic PORT

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const dbCon = require('./src/config/dbCon');
 const {app} = require('./src/server');
 const mongoose = require('mongoose');
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
     dbCon()
-})
+        .then(() => {
+            console.log('Connected to MongoDB');
+        })
+        .catch((error) => {
+            console.error('Error connecting to MongoDB:', error);
+        });
+});
